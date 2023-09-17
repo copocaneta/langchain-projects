e0d7c82d456759fa8e0af203fd04250d0749f8a0
Thiago Bernardi <copocaneta@gmail.com>
2023-09-17 13:20:10 -0300

    feat: logic to handle doc querying and querying it with llm

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -28,8 +29,8 @@ def get_text_chunks(raw_text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    # embeddings = OpenAIEmbeddings()
-    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    embeddings = OpenAIEmbeddings(client="text-embedding-ada-002")
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
@@ -43,19 +44,40 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else: 
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
 
+    st.write(css, unsafe_allow_html=True)
+
     # check if conversation is already in the state and if not initialize
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("Chat with mulitiple PDFs :books:")
-    st.text_input("Ask a question about these documents")
+    user_question = st.text_input("Ask a question about these documents")
+
+    if user_question:
+        handle_userinput(user_question)
+
+    st.write(user_template.replace("{{MSG}}", "hello robot"), unsafe_allow_html=True)
+    st.write(bot_template.replace("{{MSG}}", "hello human"), unsafe_allow_html=True)
 
     with st.sidebar:
+        # setup
         st.subheader("My Documents")
         pdf_docs = st.file_uploader("Upload PDFs here and process", accept_multiple_files=True)
         if st.button("Process"):
